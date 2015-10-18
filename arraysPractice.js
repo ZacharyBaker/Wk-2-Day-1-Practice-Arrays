3ecc8e7005a84c8140e1ab99152e4d71029a27d0
Zach Baker <zach.r.baker1@gmail.com>
2015-10-18 15:17:27 -0600

    addTen

--- arraysPractice.js
+++ arraysPractice.js
@@ -230,11 +230,19 @@ var maker = function(){
 
 
 var numbers = [5, '9', 16, 19, '25', '34', 48];
-//Write a function called addTen that is given 'numbers' as it's only argument and returns a new
-//array after adding ten to each item in numbers. *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
+//Write a function called addTen that is given 'numbers'
+// as it's only argument and returns a new
+//array after adding ten to each item in numbers. 
+//*Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
 
   //Code Here
-
+var addTen = function(numbers){
+  for (var i = 0; i < numbers.length; i++){
+   numbers[i] = parseInt(numbers[i]) + 10;
+  }
+  var newArr = numbers;
+  return newArr;
+}
 
 
 //Next Problem
